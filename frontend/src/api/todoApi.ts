2f08748d591aed188c2b1a2f5944825b4ed7f0ba
mamali03 <mamalimaharana04@gmail.com>
2026-06-24 08:34:13 +0000

    Add create todo functionality

--- frontend/src/api/todoApi.ts
+++ frontend/src/api/todoApi.ts
@@ -4,7 +4,7 @@ const API_BASE_URL ="https://fluffy-goggles-q7wr4qwjxpp2wq7-5054.app.github.dev"
 //const API_BASE_URL = "http://localhost:5054"
 
 export async function getTodos():Promise<Todo[]>{
-    console.log("getTodos started");
+   console.log("getTodos started");
    const response = await fetch(`${API_BASE_URL}/api/todos`);
 
    if(!response.ok)
@@ -13,4 +13,21 @@ export async function getTodos():Promise<Todo[]>{
    }
    console.log("response received");
    return response.json();
+}
+
+export async function createTodo(title:string):Promise<void>{
+    const response = await fetch(`${API_BASE_URL}/api/todos`,{
+        method: "POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            title:title
+        })
+    });
+
+    if(!response.ok)
+    {
+        throw new Error("Failed to create todo");
+    }
 }
--- frontend/src/pages/TodoPage.tsx
+++ frontend/src/pages/TodoPage.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { useTodos } from "../hooks/useTodos"
+import { createTodo } from "../api/todoApi";
 
 export function TodoPage()
 {
-    const {todos,loading} = useTodos();
+    const {todos,loading,reload} = useTodos();
+    const [title,setTitle] = useState("");
+
+    async function handleCreateTodo()
+    {
+      if(!title.trim())
+      {
+        return;
+      }
+
+      await createTodo(title);
+      setTitle("");
+      await reload();
+    }
+
     if(loading)
     {
       return <h1>Loading...</h1>
@@ -11,6 +27,12 @@ export function TodoPage()
     return(
       <div>
         <h1>My Todos</h1>
+
+        <div>
+          <input value={title} onChange={(e)=>{setTitle(e.target.value)}}></input>
+          <button onClick={handleCreateTodo}>Add</button>
+        </div>
+
         <ul>
           {
             todos.map((todo)=>(
